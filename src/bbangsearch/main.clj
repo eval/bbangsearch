822c62f9c00606284babdb28a06288292d2f2249
Gert Goet <gert@thinkcreate.dk>
2023-11-08 12:24:09 +0100

    Build releases

--- src/bbangsearch/main.clj
+++ src/bbangsearch/main.clj
@@ -1,10 +1,30 @@
 (ns bbangsearch.main
   (:require [babashka.cli :as cli]
+            [babashka.process :refer [process]]
             [bbangsearch.bangs :as bangs]
             [bbangsearch.util :as util]
             [clojure.java.browse :refer [browse-url]]
+            [clojure.java.io :as io]
             [clojure.string :as string]
             [selmer.parser :as selmer]))
+
+(def ^:private  current-file *file*)
+
+(defn- git [& args]
+  (some-> (apply process "git" args) :out slurp string/trimr))
+
+(defn- current-version []
+  (let [dev?    (nil? (io/resource "VERSION"))
+        bin     (if dev? "bbang-dev" "bbang")
+        version (string/trim
+                 (if dev?
+                   (let [git-dir (util/find-up ".git" current-file)]
+                     (git "--git-dir" (str git-dir) "describe" "--tags"))
+                   (slurp (io/resource "VERSION"))))]
+    (str bin " " version)))
+
+(defn- print-version []
+  (println (current-version)))
 
 (defn- bang-url [bang s]
   (selmer/render bang {:s (str s)}))
@@ -20,14 +40,12 @@
         column-atts   '(:bang :domain :desc)
         max-width     (when-not plain-mode?
                         (:cols (util/terminal-dimensions)))
-        #_#__         (prn :max max-width)
         desc-truncate #(util/truncate %1 {:truncate-to %2
                                           :omission    "…"})]
     (util/print-table column-atts printable-scripts {:skip-header          skip-header?
                                                      :max-width            max-width
                                                      :width-reduce-column  :desc
                                                      :width-reduce-fn      desc-truncate
-                                                     #_#_:column-coercions column-coercions
                                                      :no-color             no-color?})))
 
 (defn- quote-bang-args [bang-args]
@@ -52,58 +70,34 @@
   (print-bangs (printable-bangs (bangs/all)) cli-opts))
 
 (defn- handle-help []
-  (println (str "BBangSearch
+  (println (str (current-version) "
 
 A CLI for DuckDuckGo's bang searches written in Babashka.
 
 "
-            (util/bold  "USAGE" {}) "
+                  (util/bold  "USAGE" {}) "
   $ bbang [bang [terms] [--url]]
   $ bbang [COMMAND]
 
 "
-                (util/bold "OPTIONS" {}) "
+                  (util/bold "OPTIONS" {}) "
   --url  Print url instead of opening browser.
 
 "
-                (util/bold "COMMANDS" {}) "
-  bangs:ls  List all bangs (or via `bs bangs <terms>`)
+                  (util/bold "COMMANDS" {}) "
+  bangs:ls  List all bangs (or via `bbang bangs <terms>`)
 ")))
-#_((fn [& args]
-   (let [{[cmd & cmd-args :as parsed-args] :args
-          :keys [opts]} (doto (cli/parse-args args {:alias {:h :help}}) prn)]
-     (prn :cmd cmd :cmd-args cmd-args #_#_:parsed-args parsed-args)
-     (cond
-       (or (empty? args) (:help opts)) :help
-       (= cmd "bangs:ls") :bangs-ls
-       :else :bang))) "bangs:ls" "-e" "foo")
 
-#_(def ^:private cli-opts {:alias {:h :help}})
-;; '("-h" "--help" "help")
-;; "bangs:ls"
-;; '("some-bang") '("some-bang" "some-query")
 (defn -main [& args]
   (let [{[cmd & cmd-args :as _parsed-args] :args
-         :keys                            [opts]} (cli/parse-args args {:alias {:h :help}})]
+         :keys                             [opts]} (cli/parse-args args {:restrict [:version :help :url]
+                                                                         :alias    {:h :help :v :version}})]
     #_(prn :cmd cmd :cmd-args cmd-args #_#_:parsed-args parsed-args)
     (cond
       (or (empty? args) (:help opts)) (handle-help)
+      (:version opts)                 (print-version)
       (= cmd "bangs:ls")              (handle-bangs-ls cmd-args opts)
-      :else                           (handle-bang cmd cmd-args opts)))
-
-  #_(let [parsed-args (cli/parse-args args cli-opts)]
-      #_(print-bangs (printable-bangs (bangs)) {})
-      #_(let [[bang-arg & bang-q] args
-              q                   (string/join " "
-                                               (map (fn [term]
-                                                      (if (string/includes? term " ")
-                                                        (pr-str term)
-                                                        term)) bang-q))]
-          #_(prn :arg args :q q)
-          (if-let [{tpl :tpl} (get (bangs) bang-arg)]
-            (browse-url (doto (bang-url tpl q) prn))
-            (do (println (str "Unknown bang '" (first args) "'."))
-                (System/exit 1))))))
+      :else                           (handle-bang cmd cmd-args opts))))
 
 (comment
 
--- src/bbangsearch/util.clj
+++ src/bbangsearch/util.clj
@@ -3,6 +3,65 @@
             [babashka.process :as p]
             [clojure.string :as string]))
 
+(defn find-up
+  "Starting in folder `start` and traversing up, either checks if the folder contains `to-find` (returning the file if it does) or, when `to-find` is an fn, returns the path of the first folder for which `to-find` returns logical true.
+
+  - `to-find` - a string, file or path such as \"README.md\", \".\", (fs/path \"fs\") or a (fn accept [^java.nio.file.Path p]) -> truthy
+  - `start` (default `(fs/cwd)`) - a string, file or path such as \".\", \"~/projects\", (fs/file \"README.md\"). This folder or file should exist, else an `IllegalArgumentException` is thrown.
+
+  Yields the file or path found, else `nil`.
+
+  Examples:
+
+  ``` clojure
+  (fs/find-up \"README.md\") ;; search for README.md starting from CWD.
+
+  ;; find .gitignore starting from parent folder
+  (fs/find-up \".gitignore\" (fs/parent (fs/cwd)))
+  (fs/find-up \".gitignore\" \"..\")
+  (fs/find-up \"../.gitignore\")
+
+  ;; find the git work tree using a predicate
+  (let [git-work-tree? #(fs/exists? (fs/path % \".git\"))]
+    (fs/find-up git-work-tree?))
+
+  ;; find root of Clojure project we're in (if any).
+  (let [file-finder (fn [path]
+                      #(not-empty (fs/glob path %)))
+        clj-project? (some-fn (file-finder \"project.clj\") (file-finder \"deps.edn\"))]
+    (fs/find-up clj-project?))
+
+  ;; `start` may be a file
+  (fs/find-up \".gitignore\" \"~/.gitignore\") ;; => /full/path/to/home/.gitignore
+
+  ;; find all .gitignore files in CWD and ancestors
+  (let [to-find \".gitignore\"]
+    (take-while some?
+      (iterate #(fs/find-up (fs/parent to-find) %) (find-up to-find))))
+  ```
+  "
+  ([to-find] (find-up to-find (fs/cwd)))
+  ([to-find start]
+   (when (and to-find start)
+     (letfn [(below-root? [file start-folder]
+               (when-not (fn? file)
+                 (let [required-start-folder-depth (count (filter #(= ".." %) (map str (fs/normalize file))))
+                       start-folder-depth          (count (seq start-folder))]
+                   (< start-folder-depth required-start-folder-depth))))]
+       (let [start-folder        (let [expanded (fs/canonicalize (fs/expand-home (fs/path start)))]
+                                   (cond-> expanded
+                                     (fs/regular-file? expanded) fs/parent))
+             start-and-ancestors (take-while some? (iterate fs/parent (fs/normalize start-folder)))
+             folder-map-fn       (if (fn? to-find) identity #(fs/path % to-find))
+             folder-filter-fn    (if (fn? to-find) to-find fs/exists?)]
+         (when (not (fs/exists? start-folder))
+           (throw (IllegalArgumentException. (str "Folder does not exist: " start-folder))))
+         (when-not (below-root? to-find start-folder)
+           (->> start-and-ancestors
+                (map folder-map-fn)
+                (filter folder-filter-fn)
+                first)))))))
+
 (defn ensure-path-exists! [path]
   (fs/create-dirs path))
 
